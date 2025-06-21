534965c5a8a75c8d2ecd83af013c7812729896ab
junli2803 <junli81@gmx.de>
2025-06-21 10:48:40 +0200

    Delete the sailing sub-project

--- sailing/src/main/java/com/yh/sailing/common/domain/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.yh.sailing.common.domain;
-
-public interface ErrorCode {
-
-    int getCode();
-
-    String getDesc();
-
-}
--- sailing/src/main/java/com/yh/sailing/generator/VerificationKeyGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.yh.sailing.generator;
-
-/**
- * 验证key生成器
- */
-public interface VerificationKeyGenerator {
-    String generate(String prefix);
-}
